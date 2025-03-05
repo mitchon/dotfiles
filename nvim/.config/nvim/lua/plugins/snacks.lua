7369be43f0dc442c4680af57bf3c3391cb8f3323
Komarov Dmitri <komarov.da@perfema.com>
2025-03-05 18:59:17 +0300

    moving away from snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -8,21 +8,13 @@ return {
       { "echasnovski/mini.icons", version = '*' }
     },
     opts = {
-      bigfile = { enabled = true },
       dashboard = { enabled = true },
       gitbrowse = { enabled = true },
-      indent = { enabled = true },
-      input = { enabled = true },
       lazygit = { enabled = true },
       notifier = { enabled = true },
-      picker = { enabled = true },
       terminal = { enabled = true },
     },
     keys = {
-      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
-      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-
       { "<c-\\>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
       { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
       { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
